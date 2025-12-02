4f65a86d703acb7294962522a68f4b65687a9ed6
AashishUser <aashisht851236@gmail.com>
2025-12-02 21:45:18 +0545

    Create the header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,55 @@
+import { useLocation, useNavigate } from "react-router-dom";
+
+export default function Header() {
+  const location = useLocation();
+  const naviagte = useNavigate();
+  console.log(location.pathname);
+  function pathMathRoute(route) {
+    if (route === location.pathname) {
+      return true;
+    }
+  }
+
+  return (
+    <div className="bg-white border-b shadow-sm sticky top-0 z-50">
+      <header className="flex justify-between items-center px-3 max-w-6xl mx-auto">
+        <div>
+          <img
+            src=""
+            alt="logo"
+            className="h-5 cursor-pointer"
+            onClick={() => naviagte("/")}
+          />
+        </div>
+        <div>
+          <ul className="flex space-x-10">
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/") && "text-black border-b-red-500"
+              }`}
+              onClick={() => naviagte("/")}
+            >
+              Home
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/offers") && "text-black border-b-red-500"
+              }`}
+              onClick={() => naviagte("/offers")}
+            >
+              Offers
+            </li>
+            <li
+              className={`cursor-pointer py-3 text-sm font-semibold text-gray-400 border-b-[3px] border-b-transparent ${
+                pathMathRoute("/sign-in") && "text-black border-b-red-500"
+              }`}
+              onClick={() => naviagte("/sign-in")}
+            >
+              Sign in
+            </li>
+          </ul>
+        </div>
+      </header>
+    </div>
+  );
+}
--- src/pages/Offers.jsx
+++ src/pages/Offers.jsx
@@ -1,5 +1,37 @@
-export default function Offers(){
-    return(
-        <div>Offers</div>
-    )
+// export default function Offers(){
+//     return(
+//         <div>Offers</div>
+//     )
+// }
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// #region constants
+
+// #endregion
+
+// #region styled-components
+
+// #endregion
+
+// #region functions
+
+// #endregion
+
+// #region component
+const propTypes = {};
+
+const defaultProps = {};
+
+/**
+ * 
+ */
+const Offers = () => {
+    return <div>Offers</div>;
 }
+
+Offers.propTypes = propTypes;
+Offers.defaultProps = defaultProps;
+// #endregion
+
+export default Offers;
