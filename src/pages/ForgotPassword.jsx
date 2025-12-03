1052d37d7c352a70d3fe75a6cb4836c24bd26255
AashishUser <aashisht851236@gmail.com>
2025-12-03 20:44:51 +0545

    Singn Up, forgot password created

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,26 +1,15 @@
 import { useState } from "react";
-import { FaEye, FaEyeSlash } from "react-icons/fa";
 import { Link } from "react-router";
 import OAuth from "../components/OAuth";
 
-export default function SignUp() {
-  const [showPassword, setShowPassword] = useState(false);
-  const [formData, setFormData] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-  const { name, email, password } = formData;
+export default function ForgotPassword() {
+  const [email, setEmail] = useState("");
   function onChange(e) {
-    console.log(e.target.value);
-    setFormData((prevState) => ({
-      ...prevState,
-      [e.target.id]: e.target.value,
-    }));
+    setEmail(e.target.value);
   }
   return (
     <section>
-      <h1 className="text-3xl text-center mt-6 font-bold">Sign Up</h1>
+      <h1 className="text-3xl text-center mt-6 font-bold">Forgot Password</h1>
       <div className="flex justify-center flex-wrap items-center px-6 py-12 max-w-6xl mx-auto">
         <div className="w-full md:w-[67%] lg:w-[50%] mb-12 md:mb-6">
           <img
@@ -33,56 +22,28 @@ export default function SignUp() {
             <input
               className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white rounded transition ease-in-out"
               type="text"
-              id="name"
-              value={name}
-              placeholder="Full Name"
-              onChange={onChange}
-            />
-            <input
-              className="mb-6 w-full px-4 py-2 text-xl text-gray-700 bg-white rounded transition ease-in-out"
-              type="text"
               id="email"
               value={email}
               placeholder="Email Address"
               onChange={onChange}
             />
-            <div className="relative mb-6">
-              <input
-                className="w-full px-4 py-2 text-xl text-gray-700 bg-white rounded transition ease-in-out"
-                type={showPassword ? "text" : "password"}
-                id="password"
-                value={password}
-                placeholder="Password"
-                onChange={onChange}
-              />
-              {showPassword ? (
-                <FaEyeSlash
-                  className="absolute right-3 top-3 text-xl cursor-pointer"
-                  onClick={() => setShowPassword((prevState) => !prevState)}
-                />
-              ) : (
-                <FaEye
-                  className="absolute right-3 top-3 text-xl cursor-pointer"
-                  onClick={() => setShowPassword((prevState) => !prevState)}
-                />
-              )}
-            </div>
+
             <div className="flex justify-between whitespace-nowrap text-sm sm:text-lg">
               <p className="mb-6">
-                Have a account?
+                Don't have a account?
                 <Link
-                  to="/sign-in"
+                  to="/sign-up"
                   className="text-red-600 hover:text-red-800 transition duration-200 ease-in-out ml-1"
                 >
-                  Sign In
+                  Register
                 </Link>
               </p>
               <p>
                 <Link
-                  to="/forgot-password"
+                  to="/sign-in"
                   className="text-blue-600 hover:text-blue-800 transition duration-200 ease-in-out"
                 >
-                  Forgot Password?
+                  Sign In Insted
                 </Link>
               </p>
             </div>
@@ -90,7 +51,7 @@ export default function SignUp() {
               className="w-full bg-blue-600 text-white px-7 py-2 text-sm font-medium uppercase rounded shadow-md hover:bg-blue-700 transition duration-200 ease-in-out hover:shadow-lg active:bg-blue-900"
               type="submit"
             >
-              Sign Up
+              Send reset Password
             </button>
             <div
               className=" flex my-4 before:border-t  before:flex-1 items-center before:border-gray-300
